64b52800d18e41d7198e3695b114d423300045ee
Line-em <alineemily@hotmail.com>
2022-08-11 10:44:05 +0100

    Add React Router and some data files

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+export default [
+	{
+		id: "1",
+		title: "Quizzical",
+		description:
+			"Generate customized quizzes and check how you've fared! It has a dark mode, an animated svg background, and various visual feedbacks depending on user state. ",
+		tech: ["React", "Open Trivia API", "CSS Grid", "CSS animation"],
+		image: "../../../projects/quizzical.png",
+		github: "https://github.com/line-em/Quizzical",
+		live: "https://line-em.github.io/Quizzical/"
+	},
+	{
+		id: "2",
+		title: "Books E-commerce",
+		description:
+			"I decided to build an experimental e-commerce website, to be able to study some new React Hooks, such as useContext. This was a tough cookie! After making several projects using useState, and needing to both lift state up and drill it down, it was time to finally use useContext. I can see how important and useful this is, specially for a bigger project. I also used React Router Dom and had fun with CSS Grid.",
+		tech: ["React", "React Router Dom"],
+		image: "../../../public/projects/ecommerce.png",
+		github: "ss",
+		live: "ss"
+	},
+	{
+		id: "3",
+		title: "NetFlix Clone",
+		description: "Lorem ipsum dolor sit amet, consectetur",
+		tech: ["React", "React Router Dom", "Firebase", "AXIOS", "Tailwind"],
+		image: "",
+		github: "ss",
+		live: "ss"
+	},
+	{
+		id: "4",
+		title: "Weather App",
+		description:
+			"In this project I used React to create a simple weather app. I used an API, and it fetches the weather data and forecast for several days. I included error and loading states, and the background responds to the weather. The interface is inspired by an application window!",
+		tech: ["React", "Axios"],
+		image: "../../../public/projects/weatherapp.png",
+		github: "https://github.com/line-em/react-weather-app",
+		live: "https://line-em.github.io/react-weather-app/"
+	},
+	{
+		id: "5",
+		title: "ToDo App",
+		description:
+			"CHANGE IMAGE!!!! Although it is a standard project, I felt it would be important to manage state like this: creating, editing, deleting, and completing tasks. First, I made it with Vanilla JS using classes, and then I made it with React using hooks. It was wonderful to see the difference between the two, and how React streamlines the whole process!",
+		tech: ["React"],
+		image: "../../../public/projects/todo.png",
+		github: "https://github.com/line-em/react-to-do/",
+		live: "https://line-em.github.io/react-to-do/"
+	}
+];
