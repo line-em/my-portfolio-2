6c2ab8fdc137ca7d6d4e0147b8913669d4ec7bb5
Line-em <alineemily@hotmail.com>
2022-09-27 11:54:48 +0100

    css cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -39,11 +39,3 @@ function App() {
 }
 
 export default App;
-
-// FIXME: https://www.smashingmagazine.com/2020/10/introduction-to-framer-motion/
-// Carrousel animated: https://codesandbox.io/s/chakra-framer-carousel-demo-rr0dnc
-// Animated navbar: https://codesandbox.io/s/animated-menu-underline-using-animatesharedlayout-u18q7?file=/src/App.js:883-891
-// Outline navbar: https://codesandbox.io/s/framer-motion-2-animatesharedlayout-animate-between-different-components-dy0bv?file=/src/App.js
-// TODO: Tutorial framer motion navbar https://blog.maximeheckel.com/posts/framer-motion-layout-animations/ - https://blog.maximeheckel.com/posts/guide-animations-spark-joy-framer-motion/
-// Docs: https://www.framer.com/docs/animate-shared-layout/
-// https://blog.bitsrc.io/introduction-to-framer-motion-for-react-4ba5a9cabc1b
